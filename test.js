fb2ebb0f19aaec74de0820b6ee0584cd55e21f2f
JuniorEinstein <cordelia273@naver.com>
2016-07-01 11:59:44 +0900

    2016.07.01

--- test.js
+++ test.js
@@ -3,27 +3,20 @@ $(document).ready(function(){
   $('.nav-mobile, .menu-back').hide();
 
   $('.mobile-button').click(function(){
-    if(a===0)
-    {
-      $('.nav-mobile').show(1000);
-      $('.menu-back').show(1000);
-      a = 1;
-    }
-    else if(a===1)
-    {
-      $('.nav-mobile').hide(1000);
-      $('menu-back').hide(1000);
-      a = 0;
-    }
+      $('.nav-mobile, .menu-back').show();
+      $('.nav-mobile').animate({left: '0'}, 'fast')
+      $('.menu-back').animate({opacity: '0.3'}, 'fast');
   });
+
   $('.menu-back').click(function(){
-    $('.nav-mobile').hide();
-    $(this).hide();
+    $('.nav-mobile').animate({left: '-200px'}, 'fast')
+    $('.menu-back').animate({opacity: '0'}, 'fast');
+    $('.nav-mobile, .menu-back').hide(100);
     a = 0;
   });
 
   $('.nav li a').click(function(){
-    $('.nav li a.active').removeClass('active');
+    $('.nav li a.active, .nav-mobile li a.active').removeClass('active');
     $(this).addClass('active');
   });
 });
