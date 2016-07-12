1d46a0239fa6d0f60d4bee4307f28553ba49f834
JuniorEinstein <cordelia273@naver.com>
2016-07-12 21:04:13 +0900

    2016.07.12

--- test.js
+++ test.js
@@ -1,22 +1,15 @@
 $(document).ready(function(){
-  var a = 0;
-  $('.nav-mobile, .menu-back').hide();
+    $('#fullpage').fullpage({
+        anchors:['cover', 'dimigo', 'profile', 'contact'],
+        sectionsColor: ['#1ABD9D', '#E91E63', '#03a9f4', '#607D8B'],
 
-  $('.mobile-button').click(function(){
-      $('.nav-mobile, .menu-back').show();
-      $('.nav-mobile').animate({left: '0'}, 'fast')
-      $('.menu-back').animate({opacity: '0.3'}, 'fast');
-  });
+        navigation: true, navigationPosition: 'right',
+        navigationTooltips: ['ChalkPE', 'Dimigo', 'Profile', 'Contact'],
 
-  $('.menu-back').click(function(){
-    $('.nav-mobile').animate({left: '-200px'}, 'fast')
-    $('.menu-back').animate({opacity: '0'}, 'fast');
-    $('.nav-mobile, .menu-back').hide(200);
-    a = 0;
-  });
+        loopBottom: true, css3: true, scrollingSpeed: 1000
+    });
 
-  $('.nav li a').click(function(){
-    $('.nav li a.active, .nav-mobile li a.active').removeClass('active');
-    $(this).addClass('active');
-  });
+    $('#scroll-to-top').click(function(){
+        $.fn.fullpage.moveTo('cover');
+    })
 });
