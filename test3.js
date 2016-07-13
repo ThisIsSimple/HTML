a533bf089740a4ced68ae516e0ca3f000d5cb13e
JuniorEinstein <cordelia273@naver.com>
2016-07-13 22:39:22 +0900

    2016.07.13

--- test3.js
+++ test3.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
   var rand = Math.random()*10;
   if(rand<=4) {
     $('#slide-1').addClass('active');
+    $('div#section0.section.fp-section.fp-table').css("background-color", "red");
   }
   else if(rand<=7) {
     $('#slide-2').addClass('active');
+    $('div#section0.section.fp-section.fp-table').css("background-color", "blue");
   }
   else {
     $('#slide-3').addClass('active');
+    $('div#section0.section.fp-section.fp-table').css("background-color", "green");
   }
 
   var a = 0;
