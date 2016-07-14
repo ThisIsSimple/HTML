6fc7735c71bc4207f95ef5a26f811bc2a8335823
JuniorEinstein <cordelia273@naver.com>
2016-07-14 18:47:21 +0900

    2016.07.14

---
+++ DIMIGOIN/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  var rand = Math.random()*10;
+  if(rand<=4) {
+    $('#slide-1').addClass('active');
+  }
+  else if(rand<=7) {
+    $('#slide-2').addClass('active');
+  }
+  else {
+    $('#slide-3').addClass('active');
+  }
+
+  var a = 0;
+  $('.nav-mobile, .menu-back').hide();
+
+  $('.mobile-button').click(function(){
+      $('.nav-mobile, .menu-back').show();
+      $('.nav-mobile').animate({left: '0'}, 'fast')
+      $('.menu-back').animate({opacity: '0.3'}, 'fast');
+  });
+
+  $('.menu-back').click(function(){
+    $('.nav-mobile').animate({left: '-200px'}, 'fast')
+    $('.menu-back').animate({opacity: '0'}, 'fast');
+    $('.nav-mobile, .menu-back').hide(200);
+    a = 0;
+  });
+
+  $('.nav li a').click(function(){
+    $('.nav li a.active, .nav-mobile li a.active').removeClass('active');
+    $(this).addClass('active');
+  });
+});
